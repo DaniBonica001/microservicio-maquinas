a698fed95117b0541d2a02490ee69016504ecbee
G-Delgado <1006170256@u.icesi.edu.co>
2024-08-26 18:02:07 -0500

    UPDATE: Added H2 Init

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO machines (name, description, quantity) VALUES
+                                                       ('Treadmill', 'A machine for running or walking', 10),
+                                                       ('Elliptical', 'A machine for low-impact cardio workouts', 8),
+                                                       ('Dumbbells', 'Free weights for strength training', 20),
+                                                       ('Stationary Bike', 'A bike for indoor cycling', 6);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE machines (
+                          machineId BIGINT AUTO_INCREMENT PRIMARY KEY,
+                          name VARCHAR(255),
+                          description VARCHAR(255),
+                          quantity INT
+);
