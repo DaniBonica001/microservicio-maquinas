3ecbc75a9276addc4d7669c90b25af4c17f5cf89
DaniBonica001 <danielabc_0106@hotmail.com>
2024-09-15 15:50:01 -0500

    swagger documentation

--- src/main/java/co/analisys/machineservice/API/MachineAPI.java
+++ src/main/java/co/analisys/machineservice/API/MachineAPI.java
@@ -2,6 +2,9 @@ package co.analisys.machineservice.API;
 
 
 import co.analisys.machineservice.model.Machine;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,9 +17,26 @@ public interface MachineAPI {
     String BASE_URL = "/machines";
 
     @PostMapping
+    @Operation(
+            summary = "Crear una nueva máquina",
+            description = "Este endpoint permite crear una nueva máquina proporcionando los detalles de la misma."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Máquina creada exitosamente"),
+            @ApiResponse(responseCode = "400", description = "Datos de entrada no válidos"),
+            @ApiResponse(responseCode = "500", description = "Error interno del servidor")
+    })
     Machine createMachine(@RequestBody Machine machine);
 
     @GetMapping("/all")
+    @Operation(
+            summary = "Obtener todas las máquinas",
+            description = "Este endpoint permite obtener una lista de todas las máquinas registradas."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Máquinas recuperadas exitosamente"),
+            @ApiResponse(responseCode = "500", description = "Error interno del servidor")
+    })
     List<Machine> getAllMachines();
 
 }
